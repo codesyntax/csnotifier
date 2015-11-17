c52d9e19fb7e0657f9bff7742f0b9af1f3638349
Aitzol Naberan <anaberan@codesyntax.com>
2015-11-17 15:20:52 +0100

    Fix manager

--- csnotifier/models.py
+++ csnotifier/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from .notifications import send_request
 
 class DeviceManager(models.Manager):
-    def register_device(token):
+    def register_device(self,token):
         device_id = uuid.uuid4().get_hex().replace('-','')
         device = Device.objects.create(uuid=device_id,
                                        token=token)
