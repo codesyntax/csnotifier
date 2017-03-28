a050c4bc5c29f9bd2626628c6680aad2c125b334
Anartz Muxika Ledo <mugan86@gmail.com>
2017-03-28 10:31:30 +0200

    polish setup.py and add requests dependency

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ version = '0.8.dev0'
 
 setup(name='csnotifier',
       version=version,
-      description="",
-      long_description="""\
-""",
+      description="A product to send notifications to Pushwosh or Firebase",
+      long_description="""Notification sender form django applications""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      author='CodeSyntax',
+      author_email='info@codesyntax.com',
+      url='https://codesyntax.com',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          'requests'
       ],
       entry_points="""
       # -*- Entry points: -*-
