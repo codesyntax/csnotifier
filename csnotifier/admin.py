ceea2f65a9576cadda1dfcecdf212b39126f095d
Anartz Muxika Ledo <mugan86@gmail.com>
2017-03-28 10:02:36 +0200

    add actions

--- csnotifier/admin.py
+++ csnotifier/admin.py
@@ -2,13 +2,24 @@ from csnotifier.models import Notification
 from csnotifier.models import Device
 from django.contrib import admin
 
+
+def mark_as_sent(modeladmin, request, queryset):
+    queryset.update(sent=True)
+mark_as_sent.short_description = "Mark these notifications as SENT"
+
+def mark_as_not_sent(modeladmin, request, queryset):
+    queryset.update(sent=False)
+mark_as_not_sent.short_description = "Mark these notifications as NOT SENT"
+
+
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'tags', 'sent', 'data')
+    list_display = ('title', 'tags', 'data', 'sent')
+    actions = [bidalita, bidaligabe]
 
 
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('uuui', 'token', 'tags', 'enabled')
-    
+    list_display = ('uuid', 'token', 'tags', 'enabled')
+
 
 
 admin.site.register(Notification, NotificationAdmin)
